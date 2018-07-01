e6377658cc2915df89b13ba5d39070cbbae8dfcd
asrowley <asrowley@hotmail.com>
2018-07-01 13:31:10 +0100

    Added World Cup Team model, world cup teams and a list of World Cup teams in the controller and the view has a list of World Cup teams (purely as practice)

---
+++ Vidly/Controllers/WorldCupTeamController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class WorldCupTeamController : Controller
+    {
+
+
+        // GET: WorldCupTeam
+        public ActionResult Index()
+        {
+            var teams = new List<WorldCupTeam>();
+
+            var teamA = new WorldCupTeam() { Id = 1, Name = "England", StarPlayer = "Harry Kane" };
+            var teamB = new WorldCupTeam() { Id = 2, Name = "Colombia", StarPlayer = "Radamel Falcao" };
+
+            teams.Add(teamA);
+            teams.Add(teamB);
+
+            return View(teams.ToList());
+        }
+    }
+}
